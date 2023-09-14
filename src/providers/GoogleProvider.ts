8b17c82cd46efb070795bb3b51094ae725674292
huming <635315107@qq.com>
2023-09-14 19:10:08 +0800

    feat: lang split with ','

--- src/providers/GoogleProvider.ts
+++ src/providers/GoogleProvider.ts
@@ -8,7 +8,7 @@ export default class GoogleProvider extends ProviderBase {
   }
 
   async translate(text: string, lang: string): Promise<string[]> {
-    const [from, to]: string[] = lang.split('-')
+    const [from, to]: string[] = lang.split(',')
     return translate<string>(text, {
       from, to, requestFunction: fetch
     }).then(({ text }) => [text])
